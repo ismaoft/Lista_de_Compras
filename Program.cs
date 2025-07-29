c7f6d4f7623bc21c80ef0af9e30811606f42ca1f
ismaoft <ismaoft@gmail.com>
2025-07-29 15:53:01 -0600

    Ajustes en vistas producto index, login parial, footer y home.

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Producto}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
